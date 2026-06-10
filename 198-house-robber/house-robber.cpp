7f2d583ccbbf054d49cfa449099c06526e17133a
mayank10k <mayankmehta433@gmail.com>
2026-06-10 14:20:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (21.08%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int helper(int idx,vector<int>&nums,vector<int>&dp){
-        if(idx==0)return nums[0];
-        if(idx<0)return 0;
-        if(dp[idx]!=-1)return dp[idx];
-        int pick=nums[idx]+helper(idx-2,nums,dp);
-        int notPick=0+helper(idx-1,nums,dp);
-        return dp[idx]=max(pick ,notPick);  //memoixation
-
-    }
-
+    //Tabulationn --> bottom - up approacch
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,-1);
-        return helper(nums.size()-1,nums,dp);
-        
+        dp[0]=nums[0];
+
+        for(int i=1;i<n;i++){
+            int pick=nums[i];
+            if(i>1)pick+=dp[i-2];
+            int notPick=0+dp[i-1];
+
+            dp[i]=max(pick,notPick);
+        }
+        return dp[n-1];
     }
 };
