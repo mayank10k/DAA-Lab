8341effcef318997941afe11d5adb2a41a6f21bc
mayank10k <mayankmehta433@gmail.com>
2026-06-10 14:23:31 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (97.18%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    //Tabulationn --> bottom - up approacch
+    //Tabulationn --> bottom - up approacch  -->constant time complexity
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-        dp[0]=nums[0];
+        // vector<int>dp(n,-1);
+        // dp[0]=nums[0];
+        int prev=nums[0];
+        int prev2=0;
 
         for(int i=1;i<n;i++){
             int pick=nums[i];
-            if(i>1)pick+=dp[i-2];
-            int notPick=0+dp[i-1];
+            if(i>1)pick+=prev2;
+            int notPick=0+prev;
 
-            dp[i]=max(pick,notPick);
+            int curr=max(pick,notPick);
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n-1];
+        return prev;
     }
 };
