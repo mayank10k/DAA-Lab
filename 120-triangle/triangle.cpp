f4e7460babb79e7c1092d2f66a88ee908116352b
mayank10k <mayankmehta433@gmail.com>
2026-06-12 11:21:28 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (23.24%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    //memoization
-    int helper(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp){
-        if(i==triangle.size()-1)return triangle[i][j];
-        if(dp[i][j]!=1e9)return dp[i][j];
-        int down=triangle[i][j]+helper(i+1,j,triangle,dp);
-        int diagonal=triangle[i][j]+helper(i+1,j+1,triangle,dp);
-        return dp[i][j]=min(down,diagonal);
-    }
-
+    //tabulation
+    
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         vector<vector<int>>dp(n,vector<int>(n,1e9));
-        return helper(0,0,triangle,dp);
+        for(int j=0;j<n;j++){
+            dp[n-1][j]=triangle[n-1][j];
+        }
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down=triangle[i][j]+dp[i+1][j];
+                int diag=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(down,diag);
+            }
+        }
+        return dp[0][0];
     }
 };
