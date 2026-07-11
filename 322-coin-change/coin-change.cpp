122d71dfc826e206063f5476689d5efa990b9d40
Mayank Mehta <mayankmehta433@gmail.com>
2026-07-11 09:48:00 +0530

    Time: 48 ms (28.70%) | Memory: 49.4 MB (28.18%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -3,8 +3,8 @@ public:
     //memoization
     int helper(int idx,int target,vector<int>& coins,vector<vector<int>>&dp){
         if(idx==0){
-            if(target%coins[0]==0)return dp[idx][target]=target/coins[0];
-            else return dp[idx][target]=1e9;
+            if(target%coins[0]==0)return target/coins[0];
+            else return 1e9;
         }
         if(dp[idx][target]!=-1)return dp[idx][target];
         int notTake=0+helper(idx-1,target,coins,dp);
@@ -13,11 +13,10 @@ public:
         return dp[idx][target]=min(take,notTake);
     }
     int coinChange(vector<int>& coins, int amount) {
-        if(amount==0)return 0;
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        helper(n-1,amount,coins,dp);
-        return dp[n-1][amount]>=1e9?-1:dp[n-1][amount];
+        int ans=helper(n-1,amount,coins,dp);
+        return ans>=1e9?-1:ans;
 
     }
 };
