f187e06b2a1946fe5c2458b08b0530dd92791306
Mayank Mehta <mayankmehta433@gmail.com>
2026-07-11 09:57:20 +0530

    Time: 50 ms (25.97%) | Memory: 49 MB (36.04%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    //memoization
-    int helper(int idx,int target,vector<int>& coins,vector<vector<int>>&dp){
-        if(idx==0){
-            if(target%coins[0]==0)return target/coins[0];
-            else return 1e9;
-        }
-        if(dp[idx][target]!=-1)return dp[idx][target];
-        int notTake=0+helper(idx-1,target,coins,dp);
-        int take=INT_MAX;
-        if(coins[idx]<=target)take=1+helper(idx,target-coins[idx],coins,dp);
-        return dp[idx][target]=min(take,notTake);
-    }
+    //tabulation
+
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=helper(n-1,amount,coins,dp);
+
+        for(int t=0;t<=amount;t++){
+            if(t%coins[0]==0)dp[0][t]=t/coins[0];
+            else dp[0][t]=1e9;
+        }
+
+        for(int idx=1;idx<n;idx++){
+            for(int target=0;target<=amount;target++){
+                int notTake=0+dp[idx-1][target];
+                int take=INT_MAX;
+                if(coins[idx]<=target)take=1+dp[idx][target-coins[idx]];
+                dp[idx][target]=min(take,notTake);
+            }
+        }
+        int ans=dp[n-1][amount];
         return ans>=1e9?-1:ans;
 
     }
