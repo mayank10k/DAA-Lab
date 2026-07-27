319ae7705099e80caf1e068e2bbb61ddc0646619
Mayank Mehta <mayankmehta433@gmail.com>
2026-07-27 18:01:34 +0530

    Time: 4 ms (81.06%) | Memory: 14.6 MB (12.68%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int  helper(string s1,string s2,int i,int j,vector<vector<int>>&dp){
-        if(i<0)return j+1;
-        if(j<0)return i+1;
+    int  helper(string& s1,string& s2,int i,int j,vector<vector<int>>&dp){
+        if(i==0)return j;
+        if(j==0)return i;
 
         if(dp[i][j]!=-1)return dp[i][j];
-        if(s1[i]==s2[j])return dp[i][j]=helper(s1,s2,i-1,j-1,dp);
+        if(s1[i-1]==s2[j-1])return dp[i][j]=helper(s1,s2,i-1,j-1,dp);
         else{
             return dp[i][j]=min({1+helper(s1,s2,i,j-1,dp),1+helper(s1,s2,i-1,j,dp),1+helper(s1,s2,i-1,j-1,dp)});
         }
     }
     int minDistance(string word1, string word2) {
         int n=word1.size(),m=word2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return helper(word1,word2,n-1,m-1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return helper(word1,word2,n,m,dp);
     }
 };
