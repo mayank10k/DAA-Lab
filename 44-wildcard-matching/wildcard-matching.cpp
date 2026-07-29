684d1500d69583477141936990536cd4bda43513
Mayank Mehta <mayankmehta433@gmail.com>
2026-07-29 14:47:57 +0530

    Time: 36 ms (80.44%) | Memory: 54.3 MB (29.34%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool helper(string& s,string& p,int i,int j,vector<vector<int>>&dp){
+        if(i<0 &&  j<0)return true;
+        if(j<0 && i>=0)return false;
+        if(i<0 && j>=0){
+            for(int k=0;k<=j;k++){
+                if(p[k]!='*')return false;
+            }
+            return true;
+        }
+
+        if(dp[i][j]!=-1)return dp[i][j];
+
+        if(s[i]==p[j] || p[j]=='?'){
+            return dp[i][j]=helper(s,p,i-1,j-1,dp);
+        }
+        if(p[j]=='*'){
+            return dp[i][j]=helper(s,p,i-1,j,dp) || helper(s,p,i,j-1,dp);
+        }
+        return dp[i][j]=false;
+
+    }
+
+    bool isMatch(string s, string p) {
+        int n=s.length();
+        int m=p.length();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return helper(s,p,n-1,m-1,dp);
+    }
+};
