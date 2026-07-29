e4d58e9d52ef262eaa10cb5dbdbba2292c579333
Mayank Mehta <mayankmehta433@gmail.com>
2026-07-29 22:05:34 +0530

    Time: 0 ms (100.00%) | Memory: 97.3 MB (61.18%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int n=prices.size();
         int bestbuy=prices[0];
         int maxProfit=0;
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             maxProfit=max(maxProfit,prices[i]-bestbuy);
             bestbuy=min(bestbuy,prices[i]);
         }
