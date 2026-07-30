59c99ca1990a29d40f22eefbb995b9bf50b238d3
Mayank Mehta <mayankmehta433@gmail.com>
2026-07-30 15:19:38 +0530

    Time: 15 ms (14.95%) | Memory: 24.8 MB (23.83%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int helper(int idx,int buy,vector<int>&prices,vector<vector<int>>&dp){
-        if(idx==prices.size())return 0;
-
-        int profit=0;
-        if(dp[idx][buy]!=-1)return dp[idx][buy];
-        if(buy){
-            profit=max(-prices[idx]+helper(idx+1,0,prices,dp),
-                        0+helper(idx+1,1,prices,dp));
-        }else{
-            profit=max(prices[idx]+helper(idx+1,1,prices,dp),
-                        0+helper(idx+1,0,prices,dp));
-        }
-
-        return dp[idx][buy]=profit;
-
-    }
+    //tabulation
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return helper(0,1,prices,dp);
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+
+        //base case(memomization to tabulation)
+        dp[n][0]=dp[n][1]=0;
+        int profit=0;
+        for(int idx=n-1;idx>=0;idx--){
+            for(int buy=0;buy<=1;buy++){
+                if(buy){
+                    profit=max(-prices[idx]+dp[idx+1][0],
+                                0+dp[idx+1][1]);
+                }else{
+                    profit=max(prices[idx]+dp[idx+1][1],
+                                0+dp[idx+1][0]);
+                }
+                dp[idx][buy]=profit;
+            }
+        }
+        return dp[0][1];
     }
 };
