81caa4ac21ee3b87b316b32357941b856448e839
Mayank Mehta <mayankmehta433@gmail.com>
2026-08-01 10:40:50 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (64.84%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
-    //recursion
-    int helper(int idx,int buy,vector<int>&prices,vector<vector<int>>&dp){
-        if(idx>=prices.size())return 0;
-
-        if(dp[idx][buy]!=-1)return dp[idx][buy];
-
-        if(buy){
-            return dp[idx][buy]=max(-prices[idx]+helper(idx+1,0,prices,dp),
-                        helper(idx+1,1,prices,dp));
-        }else{
-            return dp[idx][buy]=max(prices[idx]+helper(idx+2,1,prices,dp),
-                        helper(idx+1,0,prices,dp));
-        }
-
-    }
+    //tabulation
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
         vector<vector<int>>dp(n+1,vector<int>(2,0));
