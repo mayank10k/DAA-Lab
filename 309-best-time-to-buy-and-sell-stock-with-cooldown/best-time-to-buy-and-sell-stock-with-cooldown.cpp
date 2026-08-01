1d11dfd7a3c438e0728467046e8dfddff8311bf8
Mayank Mehta <mayankmehta433@gmail.com>
2026-08-01 10:40:27 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (53.59%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -17,8 +17,30 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return helper(0,1,prices,dp);
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+
+        for(int buy=0;buy<=1;buy++){
+            dp[n][buy]=0;
+        }
+
+        for(int idx=n-1;idx>=0;idx--){
+            for(int buy=0;buy<=1;buy++){
+                if(buy){
+                    dp[idx][buy]=max(-prices[idx]+dp[idx+1][0],
+                                dp[idx+1][1]);
+                }else{
+                    if(idx+2>n){
+                        dp[idx][buy]=max(prices[idx]+0,
+                                dp[idx+1][0]);
+                    }else{
+                        dp[idx][buy]=max(prices[idx]+dp[idx+2][1],
+                                    dp[idx+1][0]);
+                    }
+                }
+            }
+        }
+
+        return dp[0][1];
         
     }
 };
