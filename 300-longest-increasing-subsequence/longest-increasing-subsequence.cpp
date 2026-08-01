0f5152c59b007422382cb563502fb1e4d66adb88
Mayank Mehta <mayankmehta433@gmail.com>
2026-08-01 21:53:10 +0530

    Time: 271 ms (34.46%) | Memory: 296.4 MB (22.02%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -6,9 +6,8 @@ public:
         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         for(int idx=n-1;idx>=0;idx--){
             for(int prevIdx=idx-1;prevIdx>=-1;prevIdx--){
-                int len=0+dp[idx+1][prevIdx+1];
 
-                int notTake=dp[idx+1][prevIdx+1];
+                int notTake=dp[idx+1][prevIdx+1];  //prevIdx+1 for handliing -ve idx
 
                 int take=0;
                 if(prevIdx==-1 || nums[idx]>nums[prevIdx]){
